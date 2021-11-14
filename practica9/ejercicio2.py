af05ce6ab456a2b2ceb64bf29d5653a00387aa79
mayloo89 <sebastiansourigues@gmail.com>
2021-11-14 20:24:05 -0300

    added try except blocs ti practice 9

--- practica9/ejercicio2.py
+++ practica9/ejercicio2.py
@@ -1,26 +1,29 @@
 from pilacola import *
 
 def main():
-    #Inicializo pila
-    pila = inicializarpila()
-    
-    #El usuario carga elementos en pila
-    while pilavacia(pila):
-        elemento = int(input("Ingrese un elemento o -1 para finalizar: "))
-
-        while elemento != -1:
-            apilar(pila, elemento)
+    try:
+        #Inicializo pila
+        pila = inicializarpila()
+        
+        #El usuario carga elementos en pila
+        while pilavacia(pila):
             elemento = int(input("Ingrese un elemento o -1 para finalizar: "))
 
-    #Invierte el orden de pila
-    aux = inicializarpila()
-    while not pilavacia(pila):
-        apilar(aux, tope(pila))
-        desapilar(pila)
+            while elemento != -1:
+                apilar(pila, elemento)
+                elemento = int(input("Ingrese un elemento o -1 para finalizar: "))
 
-    #Imprime pila invertida
-    while not pilavacia(aux):
-        print(tope(aux), end=" ")
-        desapilar(aux)
+        #Invierte el orden de pila
+        aux = inicializarpila()
+        while not pilavacia(pila):
+            apilar(aux, tope(pila))
+            desapilar(pila)
+
+        #Imprime pila invertida
+        while not pilavacia(aux):
+            print(tope(aux), end=" ")
+            desapilar(aux)
+    except ValueError:
+        print("Error de cola vacía.")
 
 main()
